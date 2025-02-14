4d5e6f1b66adbf4d43be916a31c3f59882fda23b
Ddhruv <54676859+Ddhruv-IOT@users.noreply.github.com>
2025-02-14 23:40:59 +0530

    added solution

---
+++ Fixing Two nodes of a BST/solution.py
@@ -0,0 +1,28 @@
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+
+class Fix:
+    def __init__(self):
+        self.first=None
+        self.second=None
+        self.pred=Node(0)
+class Solution:
+    def solve(self,root,f):
+        if root:
+            self.solve(root.left,f)
+            if root.data<f.pred.data:
+                if not f.second:
+                    f.second=f.pred
+                f.first=root
+            f.pred=root
+            self.solve(root.right,f)
+    
+    def correctBST(self, root):
+        f=Fix()
+        self.solve(root,f)
+        f.first.data,f.second.data=f.second.data,f.first.data
+...# } Driver Code Ends
+class Node:
