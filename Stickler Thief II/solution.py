abe6ee4206dfe1a698d129add4c380ef0c37ade1
Ddhruv <54676859+Ddhruv-IOT@users.noreply.github.com>
2025-03-23 00:05:26 +0530

    added solution

---
+++ Stickler Thief II/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxValue(self, arr):
+        # code here
+        def rob(arr):
+            chosen, notchosen = 0, 0
+            for e in arr:
+                n_chosen = notchosen + e
+                n_notchosen = max(chosen, notchosen)
+                chosen, notchosen = n_chosen, n_notchosen
+            
+            return max(chosen, notchosen)
+        return max(rob(arr[:-1]), rob(arr[1:]))
+        
+
+...# } Driver Code Ends
